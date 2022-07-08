24c9ef205a02c7a0f09b239b279a42398ad61ae8
Pradip Bhusnar <pradip.bhusnar@tango340b.com>
2022-07-08 20:33:28 +0530

    docs(core): multiselect custom component option changes
    affects: @medly-components/core

--- packages/core/src/components/MultiSelect/MultiSelect.stories.tsx
+++ packages/core/src/components/MultiSelect/MultiSelect.stories.tsx
@@ -1,13 +1,16 @@
 import { defaultTheme, MultiSelectTheme } from '@medly-components/theme';
-import { MultiSelectProps } from './types';
 import type { FC } from 'react';
+import Box from '../Box';
+import Text from '../Text';
+import { StyledBox } from './MultiSelect.styled';
+import { MultiSelectProps } from './types';
 
 export const variants: Required<MultiSelectProps>['variant'][] = ['outlined', 'filled', 'fusion'];
 export const sizes: Required<MultiSelectProps>['size'][] = ['S', 'M'];
 
 export const options = [
         { value: 'lorem pharmacy', label: 'Lorem Pharmacy' },
-        { value: 'ipsum pharmacy', label: 'Ipsum Pharmacy' },
+        { value: 'ipsum pharmacy', label: 'Ipsum Pharmacy', creatable: true },
         {
             label: 'Group',
             value: [
@@ -16,6 +19,36 @@ export const options = [
                 { value: 'c pharmacy', label: 'c Pharmacy' },
                 { value: 'd pharmacy', label: 'd Pharmacy' }
             ]
+        }
+    ],
+    optionsWithCustomComponent = [
+        {
+            value: 'lorem pharmacy',
+            label: 'Lorem Pharmacy',
+            component: (
+                <StyledBox px={0} py={0} bg="transparent">
+                    <Text textWeight="Medium">Lorem Pharmacy</Text>
+                    <Text>Lorem pharmacy description</Text>
+                </StyledBox>
+            ),
+            componentContent: ['Lorem Pharmacy', 'Lorem pharmacy description']
+        },
+        {
+            value: 'ipsum pharmacy',
+            label: 'Ipsum Pharmacy',
+            component: (
+                <Box px={0} py={0} bg="transparent">
+                    <StyledBox px={0} py={0} bg="transparent">
+                        <Text textWeight="Medium">Ipsum Pharmacy</Text>
+                        <Text>Ipsum pharmacy description</Text>
+                    </StyledBox>
+                    <StyledBox px={0} py={0} bg="transparent">
+                        <Text textWeight="Medium">Ipsum Pharmacy1</Text>
+                        <Text>Ipsum pharmacy1 description</Text>
+                    </StyledBox>
+                </Box>
+            ),
+            componentContent: ['Ipsum Pharmacy', 'Ipsum Pharmacy1', 'Ipsum pharmacy description', 'Ipsum pharmacy1 description']
         }
     ],
     disabledOptions = [{ value: 'disabled pharmacy', label: 'Disabled Pharmacy', disabled: true }, ...options];
