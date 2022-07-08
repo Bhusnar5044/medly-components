5143bd6ed0529024d1bfbf1bbc6fd16cb3b3a157
Pradip Bhusnar <pradip.bhusnar@tango340b.com>
2022-07-08 12:41:54 +0530

    WIP(core): custom component option for SingleSelect
    affects: @medly-components/core

--- packages/core/src/components/SingleSelect/SingleSelect.stories.tsx
+++ packages/core/src/components/SingleSelect/SingleSelect.stories.tsx
@@ -1,5 +1,8 @@
 import { defaultTheme, SingleSelectTheme } from '@medly-components/theme';
 import type { FC } from 'react';
+import Box from '../Box';
+import Text from '../Text';
+import { StyledBox } from './SingleSelect.styled';
 import { Option, SingleSelectProps } from './types';
 
 export const variants: Required<SingleSelectProps>['variant'][] = ['outlined', 'filled', 'flat', 'fusion'];
@@ -25,6 +28,53 @@ export const options = [
     { value: 'Dummy4 option', label: 'Dummy4 option' },
     { value: 'Dummy5 option', label: 'Dummy5 option' },
     { value: 'Dummy6 option', label: 'Dummy6 option' }
+];
+
+export const customComponentOptions = [
+    { value: 'all', label: 'All' },
+    {
+        value: 'Dummy1 option',
+        label: 'Dummy1 option',
+        disabled: true,
+        component: (
+            <Box px={0} py={0} bg="transparent">
+                <StyledBox bg="transparent">
+                    <Text textVariant="body2" textWeight="Medium" wrap="nowrap">
+                        Dummy1 option title 1
+                    </Text>
+                    <Text textVariant="body3">Dummy description</Text>
+                </StyledBox>
+                <StyledBox bg="transparent">
+                    <Text textVariant="body2" textWeight="Medium" wrap="nowrap">
+                        Dummy1 option title 2
+                    </Text>
+                    <Text textVariant="body3">Dummy description</Text>
+                </StyledBox>
+            </Box>
+        ),
+        componentContent: ['Dummy1 option title 1', 'Dummy1 option title 2', 'Dummy description']
+    },
+    {
+        value: 'Dummy2 option',
+        label: 'Dummy2 option',
+        component: (
+            <Box px={0} py={0} bg="transparent">
+                <StyledBox bg="transparent">
+                    <Text textVariant="body2" textWeight="Medium" wrap="nowrap">
+                        Dummy2 Option title 1
+                    </Text>
+                    <Text textVariant="body3">Dummy description</Text>
+                </StyledBox>
+                <StyledBox bg="transparent">
+                    <Text textVariant="body2" textWeight="Medium" wrap="nowrap">
+                        Dummy2 Option title 2
+                    </Text>
+                    <Text textVariant="body3">dummy description</Text>
+                </StyledBox>
+            </Box>
+        ),
+        componentContent: ['Dummy2 Option title 1', 'Dummy2 Option title 2', 'dummy description']
+    }
 ];
 
 export const ThemeInterface: FC<SingleSelectTheme> = () => null;
